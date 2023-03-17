6094e2671f5f549cae1c6ad22f87311f8173d511
Wendy Amarul <42637876+wendyamarul@users.noreply.github.com>
2023-03-18 04:19:54 +0700

    main

---
+++ baseikal/command/allmenu/allmenu.js
@@ -0,0 +1,29 @@
+const allmenu =  ` 
+*BOT AEON BOT*
+*Random*
+┏━━⊱
+┣❏kalkulator
+┣❏toimage
+┣❏toonce
+┣❏tomp4
+┣❏tovn
+┣❏togif
+┣❏tourl 
+┣❏toaud
+┣❏sticker
+┣❏stickergif
+┣❏emojimix
+┣❏emojimix2
+┣❏ktpmaker
+┣❏afk
+┣❏* [ reply ]
+┣❏getname [ reply ]
+┣❏getpic [ reply ]
+┣❏infochat
+┣❏q [ reply ]
+┣❏del [ reply ]
+┣❏style [ text nya ]
+┣❏ss [link nya]
+┣❏penjara [ reply ]
+┗━━⊱[ AEON BOT]`
+exports.allmenu = allmenu
